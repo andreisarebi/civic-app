c46f7d8f77df61459dce68f4fa38d26c21769dfc
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-08-09 16:14:06 -0700

    Add About Component. Import into index.

--- src/screens/CandidateDetail/index.js
+++ src/screens/CandidateDetail/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { ScrollView, Text, StyleSheet } from 'react-native';
 import CandidateDetail from './CandidateDetailContainer';
 import PropTypes from 'prop-types';
+import About from './About';
 
 class ElectionsScreen extends React.Component {
   static navigationOptions = {
@@ -14,15 +15,7 @@ class ElectionsScreen extends React.Component {
     return (
       <ScrollView style={styles.container}>
         <CandidateDetail candidateId={this.props.navigation.state.params.id}/>
-        <Text style={styles.text}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
-        </Text>
+        <About/>
       </ScrollView>
     );
   }
@@ -32,10 +25,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-  },
-  text: {
-    padding: 10,
-  },
+  }
 });
 
 const propTypes = {
