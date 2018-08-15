93a51fef651213d47d5699f301dc3eee1a82fecd
Marc Aaron <marc.aaron.glasser@gmail.com>
2018-08-15 08:48:36 -0700

    encapsulate NewsCard into own component

--- src/screens/CandidateDetail/TabBar/NewsTab.js
+++ src/screens/CandidateDetail/TabBar/NewsTab.js
@@ -1,58 +1,23 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet } from 'react-native';
-import { Icon } from 'react-native-elements'
-import Colors from '../../../styles/colors';
-import Mixins from '../../../styles/mixins';
+import {View } from 'react-native';
+import NewsCard from './NewsCard';
+
+const testImage = require('../../../assets/images/gavin.png');
+
+const newsItems = [
+  {
+    id: 1,
+    title: 'Does Gavin Newsom represent a shift in California Democratic Party?',
+    img: testImage,
+    createdAt: '3 hrs ago', // change to datetime
+  }
+]
 
 const NewsTab = (props) => {
   return (
     <View>
-      <View style={styles.newsCard}>
-        <Image
-          source={require('../../../assets/images/gavin.png')}
-          style={styles.newsImage}
-        />
-        <View style={styles.newsBody}>
-          <Text style={styles.newsBodyText}>Does Gavin Newsom represent a shift in California Democratic Party?</Text>
-          <View style={styles.dateTag}>
-            <Icon
-              size={14}
-              name="clock"
-              type="material-community"
-              iconStyle={{marginRight: 5}}
-            />
-            <Text>3 hr ago</Text>
-          </View>
-        </View>
-      </View>
+      {newsItems.map(({id, ...rest})=><NewsCard key={id} {...rest}/>)}
     </View>);
 }
-
-const styles = StyleSheet.create({
-  newsCard: {
-    flex: 1,
-    flexDirection: 'row',
-    margin: 10,
-    borderRadius: 2,
-    ...Mixins.shadow
-  },
-  newsImage: {
-    width:175,
-    height: 140
-  },
-  newsBody:{
-    flex:1,
-    padding: 10,
-    backgroundColor: Colors.white
-  },
-  newsBodyText:{
-    fontSize: 16,
-    lineHeight: 22
-  },
-  dateTag:{
-    flexDirection: 'row',
-    marginTop: 5
-  }
-})
 
 export default NewsTab;
