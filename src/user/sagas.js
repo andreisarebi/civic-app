07b2e38653fefc3e3f7b01b423c5a7f6f457dd7a
gmferland <gmferland@gmail.com>
2018-09-24 22:54:50 -0400

    load user info on login

--- src/user/sagas.js
+++ src/user/sagas.js
@@ -4,19 +4,19 @@ import { logOutSaga } from '../auth/sagas';
 import { fetchUser } from './api';
 import { UserActionType, userFetchSuccess } from './redux';
 
-export const loadUserDataSaga = function*() {
+export const loadUserDataSaga = function*(passedInUserId) {
   try {
-    const userId = yield select(getLoggedInUserId);
+    let userId = yield select(getLoggedInUserId);
+    // use passed in value as default if no id in state
+    userId = userId || passedInUserId;
     if (userId) {
       const userResponse = yield call(fetchUser, userId);
-      yield put(userFetchSuccess(userResponse))
+      yield put(userFetchSuccess(userResponse));
     } else {
-      yield call(logOutSaga)
+      yield call(logOutSaga);
     }
-
   } catch (err) {
     // TODO: handle error
-
   }
 };
 
